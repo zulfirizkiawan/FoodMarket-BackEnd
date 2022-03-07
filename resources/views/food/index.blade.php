7ce934e535f5df05c749c19bbb5adfc214036364
zulfirizkiawan <zulfi.rizkiawan.27@gmail.com>
2022-03-07 18:45:32 +0700

    Food Done

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User') }}
+            {{ __('Food') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
            <div class="mb-18">
-                <a href="{{ route('users.create') }}" class="text-white font-bold py-2 px-4 rounded" style="background-color: #22b43a;">
-                    + Create User
+                <a href="{{ route('food.create') }}" class="text-white font-bold py-2 px-4 rounded" style="background-color: #22b43a;">
+                    + Create Food
                 </a>
            </div>
            <div class="bg-white mt-8">
@@ -17,28 +17,26 @@
                 <thead>
                 <tr>
                     <th class="border px-6 py-4">ID</th>
-                    <th class="border px-6 py-4">Image</th>
                     <th class="border px-6 py-4">Name</th>
-                    <th class="border px-6 py-4">Email</th>
-                    <th class="border px-6 py-4">Roles</th>
+                    <th class="border px-6 py-4">Price</th>
+                    <th class="border px-6 py-4">Rate</th>
+                    <th class="border px-6 py-4">Type</th>
                     <th class="border px-6 py-4">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                    @forelse($user as $item)
+                    @forelse($food as $item)
                         <tr>
                             <td class="border px-6 py-4 text-center">{{ $item->id }}</td>
-                            <td class="border px-6 py-2 ">
-                                <img src="{{ url('assets/user').$item->profile_photo_path }}" class="rounded" style="width: 50px">    
-                            </td>
                             <td class="border px-6 py-4 ">{{ $item->name }}</td>
-                            <td class="border px-6 py-4">{{ $item->email }}</td>
-                            <td class="border px-6 py-4">{{ $item->roles }}</td>
-                            <td class="border px-6 py-4" style=" display:flex; align-items:center; justify-content: space-evenly;">
-                                    <a href="{{ route('users.edit', $item->id) }}" class="text-white font-bold py-2 px-4 rounded" style="background-color: #2666CF;">
+                            <td class="border px-6 py-4">{{ number_format($item->price) }}</td>
+                            <td class="border px-6 py-4">{{ $item->rate }}</td>
+                            <td class="border px-6 py-4">{{ $item->types }}</td>
+                            <td class="border px-6 py-3" style=" display:flex; align-items:center; justify-content: space-evenly;">
+                                    <a href="{{ route('food.edit', $item->id) }}" class="text-white font-bold py-2 px-4 rounded" style="background-color: #2666CF;">
                                         Edit
                                     </a>
-                                    <form action="{{ route('users.destroy', $item->id) }}" method="POST" class="inline-block">
+                                    <form action="{{ route('food.destroy', $item->id) }}" method="POST" class="inline-block">
                                         {!! method_field('delete') . csrf_field() !!}
                                         <button type="submit" class="text-white font-bold py-2 px-4 rounded inline-block" style="background-color: #DD4A48;">
                                             Delete
@@ -57,7 +55,7 @@
             </table>
         </div>
         <div class="text-center mt-5">
-            {{ $user->links() }}
+            {{ $food->links() }}
         </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FoodController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ Route::prefix('dashboard')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
+        Route::resource('food', FoodController::class);
     });
 
 
